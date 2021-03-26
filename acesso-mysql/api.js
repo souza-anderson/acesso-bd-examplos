cb0252b3ca305e47f2ac498a0bbee409f0b73919
Anderson Souza <andersonfebs@gmail.com>
2021-03-26 17:32:23 -0400

    adding products mysql

--- acesso-mysql/api.js
+++ acesso-mysql/api.js
@@ -1,9 +1,14 @@
-const db = require('mysql2/promise')
+const db = require('./db')
 const categories = require('./categories')(db)
+const products = require('./products')(db)
 
 const test = async () => {
   try {
-    await categories.create(['new category from api'])
+    // await categories.create(['new category from api'])
+    // await categories.update(6, ['update category from api'])
+    // await categories.remove(6)
+    // const cats = await categories.findAll()
+    // console.log(cats)
     
   } catch (err) {
     console.log(err)
--- acesso-mysql/categories.js
+++ acesso-mysql/categories.js
@@ -3,9 +3,25 @@ const init = connection => {
     const conn = await connection
     await conn.query(`insert into categories (category) values (?)`, data)
   }
+  const update = async(id, data) => {    
+    const conn = await connection
+    await conn.query(`update categories set category = ? where id = ?`, [...data, id])
+  }
+  const remove = async (id) => {
+    const conn = await connection
+    await conn.query(`delete from categories where id = ? limit 1`, [id])
+  }
+  const findAll = async () => {
+    const conn = await connection
+    const [ result ] = await conn.query(`select * from categories`)
+    return result
+  }
 
   return {
-    create
+    create,
+    update,
+    remove,
+    findAll
   }
 }
 module.exports = init
